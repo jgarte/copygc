7af326cf1eed602b6c8ff3d8559c244589b547d8
jgart <jgart@dismail.de>
2025-01-25 12:43:01 -0600

    This works yay but...

--- simple.c
+++ simple.c
@@ -112,11 +112,7 @@ typedef uint8_t u8;
 
 void single_step(bool *running, u8 *byte_code, u8 *instruction_counter) {
     u8 opcode = byte_code[*instruction_counter];
-    printf("%p\n", instruction_counter);
-    printf("%u\n", *instruction_counter);
-    ++instruction_counter;
-    printf("%p\n", instruction_counter);
-    printf("%u\n", *instruction_counter);
+    ++*instruction_counter;
     switch (opcode) {
     case Opcode_NOOP:
         puts("NOOP");
